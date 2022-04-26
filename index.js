ac6b4a61577b82e0808e5675f549462bf52c66b8
ON Joseph <on.josephinfo@gmail.com>
2022-04-26 16:09:21 +0100

    add ES6 index.js codes

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+import { DateTime } from './node_modules/luxon/src/luxon.js';
+import { addBook, getStorageData, displayBooks } from './modules/books.js';
+import Library from './modules/lists.js';
+import {
+  displayList,
+  displayAdd,
+  displayContact,
+} from './modules/navigations.js';
 
+const Listli = document.getElementById('List');
+const Addli = document.getElementById('AddNew');
+const Contactli = document.getElementById('ContactPage');
+
+const library = new Library();
+library.awesomeBooks = [];
+
+window.onbeforeunload = () => {
+  getStorageData(library);
+  displayBooks(library);
+};
+
+const currentTime = DateTime.now().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+document.getElementById('date').innerHTML = currentTime;
+
+window.onload = displayList;
+const addBtn = document.querySelector('#add-btn');
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBook(library);
+});
+getStorageData(library);
+displayBooks(library);
+
+Listli.addEventListener('click', displayList);
+Addli.addEventListener('click', displayAdd);
+Contactli.addEventListener('click', displayContact);
